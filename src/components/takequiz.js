c093080905596302d2b275a4e5b6920f86e3f969
fong1980 <fong1980@gmail.com>
2018-06-05 17:37:09 +0200

    succeed to add component & data

---
+++ src/components/takequiz.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from "react";
+//import { connect } from "react-redux";
+import { Quiz } from "../lib/data.js";
+console.log(Quiz);
+class TakeQuiz extends PureComponent {
+  render() {
+    return (
+      // this.props.data.map()
+      <div>
+        <h1>All Advertisements:showme</h1>
+      </div>
+    );
+  }
+}
+export default TakeQuiz;
---
+++ src/lib/data.js
@@ -0,0 +1,39 @@
+export const Quiz = [
+  //radio buttons
+  {
+    id: 1,
+    quetions: "this is a quetion",
+    a: "White sauce",
+    b: "horse",
+    c: "chicken",
+    d: "rat",
+    correct_answer: "a"
+  },
+  {
+    id: 2,
+    quetions: "this is a quetion2",
+    a: "Red sauce",
+    b: "Mix it up",
+    c: "horse",
+    d: "rat",
+    correct_answer: "a"
+  },
+  {
+    id: 3,
+    quetions: "this is a quetion3",
+    a: "UFO",
+    b: "horse",
+    c: "rat",
+    d: "Double red sauce",
+    correct_answer: "a"
+  },
+  {
+    id: 4,
+    quetions: "this is a quetion4",
+    a: "Mix it up",
+    b: "horse",
+    c: "horse",
+    d: "rat",
+    correct_answer: "a"
+  }
+];
