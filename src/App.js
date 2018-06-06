e88c726d6d32f500ae0ebc78909fe83dd1d8b5e3
Zefevr <zefevillalba@gmail.com>
2018-06-06 13:00:52 +0200

    adding materialUI,  AppBar component, icon tab

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Quiz from "./components/QuestionsList";
+import AppBar from './components/AppBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title" />
-        </header>
+        <AppBar />
         <Quiz />
       </div>
     );
