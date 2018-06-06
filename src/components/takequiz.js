c9b2b6f5e1a3f7e7142fe2cbbbd658bf04cf2dd5
fong1980 <fong1980@gmail.com>
2018-06-06 12:06:40 +0200

    make a map in a map, small progress

--- src/components/takequiz.js
+++ src/components/takequiz.js
@@ -1,15 +1,59 @@
 import React, { PureComponent } from "react";
 //import { connect } from "react-redux";
 import { Quiz } from "../lib/data.js";
-console.log(Quiz);
+
 class TakeQuiz extends PureComponent {
   render() {
     return (
       // this.props.data.map()
       <div>
-        <h1>All Advertisements:showme</h1>
+        <h2>:showme</h2>
+
+        {Quiz.map((quetions, i) => (
+          <div>
+            {console.log(Object.keys(quetions))}
+            {Object.keys(quetions).map((x, i) => {
+              console.log(x);
+              // if (i > 1 && i < 6) {
+              //   return console.log(x);
+              //}
+            })}
+
+            <div key={i}>
+              {quetions.quetions}
+              <br />
+              <input name="i" type="radio" value={quetions.a} />
+              {quetions.a}
+              <br />
+              <input name="i" type="radio" value={quetions.b} />
+              {quetions.b} <br />
+              <input name="i" type="radio" value={quetions.c} />
+              {quetions.c} <br />
+              <input name="i" type="radio" value={quetions.d} />
+              {quetions.d} <br />
+              <br />
+            </div>
+          </div>
+        ))}
       </div>
     );
   }
 }
 export default TakeQuiz;
+
+// <input
+// name="choseBase"
+// type="radio"
+// value="8.99"
+// onChange={this.handleInputChange}
+// />25cm NY Style € 8,99<br />
+
+// id: 2,
+// quetions: "this is a quetion2",
+// a: "Red sauce",
+// b: "Mix it up",
+// c: "horse",
+// d: "rat",
+// correct_answer: "a"
+
+//  {console.log(Object.keys(quetions))}
--- src/lib/data.js
+++ src/lib/data.js
@@ -2,38 +2,38 @@ export const Quiz = [
   //radio buttons
   {
     id: 1,
-    quetions: "this is a quetion",
-    a: "White sauce",
-    b: "horse",
-    c: "chicken",
-    d: "rat",
+    quetions: "quetion 1",
+    a: "a White sauce ",
+    b: "b horse",
+    c: "c chicken",
+    d: "d rat",
     correct_answer: "a"
   },
   {
     id: 2,
-    quetions: "this is a quetion2",
-    a: "Red sauce",
-    b: "Mix it up",
-    c: "horse",
-    d: "rat",
+    quetions: "quetion 2",
+    a: "a Red sauce",
+    b: "b Mix it up",
+    c: "c horse",
+    d: "d rat",
     correct_answer: "a"
   },
   {
     id: 3,
-    quetions: "this is a quetion3",
-    a: "UFO",
-    b: "horse",
-    c: "rat",
-    d: "Double red sauce",
+    quetions: "quetion 3",
+    a: "a UFO",
+    b: "b horse",
+    c: "c rat",
+    d: "d Double red sauce",
     correct_answer: "a"
   },
   {
     id: 4,
-    quetions: "this is a quetion4",
-    a: "Mix it up",
-    b: "horse",
-    c: "horse",
-    d: "rat",
+    quetions: "quetion 4",
+    a: "a Mix it up",
+    b: "b horse",
+    c: "c horse",
+    d: "d rat",
     correct_answer: "a"
   }
 ];
