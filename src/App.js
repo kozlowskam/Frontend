a9e6e84c72547ea94b97f456345e358f9a801134
fong1980 <fong1980@gmail.com>
2018-06-07 12:42:03 +0200

    fronten rules

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-
 import React, { Component } from "react";
 import "./App.css";
 import Quiz from "./components/QuestionsList";
 
-import Login from "./components/LoginComponent"
-import SignUp from "./components/SignUpComponent"
+import Login from "./components/LoginComponent";
+import SignUp from "./components/SignUpComponent";
 import AppBar from "./components/AppBar";
-import QuizzesList from './components/quizzeslist';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
-
-
+import QuizzesList from "./components/quizzeslist";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import takequiz from "./components/takequiz.js";
 
 class App extends Component {
   render() {
     return (
-      
       <Router>
         <div className="App">
-      <AppBar />
+          <AppBar />
           <Route exact path="/quizzes" component={QuizzesList} />
-          <Route exact path="/quizzes/:id" />
+          <Route exact path="/quizzes/:id" component={takequiz} />
           <Route exact path="/quizzes/createquiz" component={Quiz} />
         </div>
       </Router>
-
     );
   }
 }
