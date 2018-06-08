74d692fff0f60481be264327fac589b2f72c00e8
fong1980 <fong1980@gmail.com>
2018-06-08 13:07:01 +0200

    adam rules

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-
 import React, { Component } from "react";
 import "./App.css";
 
 import QuestionList from "./components/QuestionsList";
 import CreateQuiz from "./components/CreateQuiz";
-import LoginComponent from "./components/LoginComponent"
-import SignUpComponent from "./components/SignUpComponent"
+import LoginComponent from "./components/LoginComponent";
+import SignUpComponent from "./components/SignUpComponent";
 import AppBar from "./components/AppBar";
 import QuizzesList from "./components/quizzeslist";
+import TakeQuiz from "./components/takequiz";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import Takequiz from "./components/takequiz";
@@ -15,21 +15,16 @@ import Takequiz from "./components/takequiz";
 class App extends Component {
   render() {
     return (
-
       <Router>
         <div className="App">
-
-
           <AppBar />
-        
-    
+
           <Route exact path="/createquiz" component={CreateQuiz} />
           <Route exact path="/createquestions" component={QuestionList} />
-          <Route exact path="/quizzes/:id" component={Takequiz} />
+          <Route exact path="/quizzes/:id" component={TakeQuiz} />
           <Route exact path="/login" component={LoginComponent} />
           <Route exact path="/signUp" component={SignUpComponent} />
           <Route exact path="/quizzes" component={QuizzesList} />
-
         </div>
       </Router>
     );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -9,6 +9,5 @@ export default {
   quiz,
   quizzes,
   fetchquiz,
-  questions,
   takeQuiz
 };
